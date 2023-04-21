ac3564c753a919bd79144474961058a783c61dc9
Docker69 <benny.kritzer@gmail.com>
2023-04-21 19:10:29 -0400

    Add MongoDB container

---
+++ mongodb/mongo-init.sh
@@ -0,0 +1,20 @@
+set -e
+
+mongosh <<EOF
+
+print("============= Started INIT script =======================");
+
+db = db.getSiblingDB('$MONGO_INITDB_DATABASE')
+
+db.createUser({
+  user: '$MONGO_USER_NAME',
+  pwd: '$MONGO_USER_PASSWORD',
+  roles: [{ role: 'readWrite', db: '$MONGO_INITDB_DATABASE' }],
+});
+
+db.createCollection('chats')
+db.createCollection('config')
+
+print("============= Ended INIT script =======================");
+
+EOF
